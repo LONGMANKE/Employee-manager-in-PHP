5f5a4be2f46a4bdde77b3a3f28865755475ce9dd
LONGMANKE <mburunjoroge0@gmail.com>
2022-11-23 12:37:10 +0000

    change the app

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -7,9 +7,9 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">React Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">I'm an React component!</div>
                     </div>
                 </div>
             </div>
@@ -19,8 +19,8 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    const Index = ReactDOM.createRoot(document.getElementById("example"));
+if (document.getElementById('employeeApp')) {
+    const Index = ReactDOM.createRoot(document.getElementById("employeeApp"));
 
     Index.render(
         <React.StrictMode>
